569a24ac81f985a45d63e886c6e23a6ee16e4106
Gavin Macaulay <gavin@macaulay.co.nz>
2021-10-20 15:05:00 +1300

    initial version

---
+++ do_find_argo.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 17:38:16 2021
+
+@author: gavin
+"""
+
+from pathlib import Path
+from netCDF4 import Dataset
+import numpy.ma as ma
+import numpy as np
+from cftime import num2date
+import pandas as pd
+
+dataDir = Path(r'C:\Users\gavin\Data - not synced\temp\argo_download')
+outputDir = Path(r'C:\Users\gavin\OneDrive - Aqualyd Limited\Documents\Aqualyd\Projects\2021-05 SIO ORH survey analysis\Data')
+
+profiles = dataDir.glob('**/*_prof.nc')
+
+first = True
+for p in profiles:
+    print(p.name)
+    rootgrp = Dataset(p, "r")
+    if first:
+        lats = rootgrp['LATITUDE'][:]
+        lons = rootgrp['LONGITUDE'][:]
+        units = rootgrp['JULD'].units        
+        t = rootgrp['JULD'][:]
+        time = num2date(t, units = units, calendar='julian')
+        first = False
+    else:
+        lats = ma.append(lats, rootgrp['LATITUDE'][:])
+        lons = ma.append(lons, rootgrp['LONGITUDE'][:])
+        t = rootgrp['JULD'][:]
+        time = ma.append(time, num2date(t, units=units, calendar='julian'))
+        
+    rootgrp.close()
+    
+# Now find profiles that are with the months of June to August
+start_month = 6
+end_month = 8
+start_year = 2018
+end_year = 2021
+
+months = np.array([t.month for t in time.data])
+years = np.array([t.year for t in time.data])
+
+m = (months >= start_month) & (months <= end_month) & (years >= start_year) & (years <= end_year)
+
+# and write to a csv for loading into a GIS
+d = pd.DataFrame({'timestamp': time.data[m], 'month': months[m], 'year': years[m], 'lat': lats[m], 'lon': lons[m]})
+d.to_csv(outputDir.joinpath('argo_positions.csv'), index=False)
