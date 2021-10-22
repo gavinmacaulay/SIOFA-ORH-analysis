a95e7fe8f20dc47744bdab58285fb79d9ced2965
Gavin Macaulay <gavin@macaulay.co.nz>
2021-10-22 14:36:38 +1300

    Add cycle_number and filepath to output

--- do_find_argo.py
+++ do_find_argo.py
@@ -27,12 +27,17 @@ for p in profiles:
         units = rootgrp['JULD'].units        
         t = rootgrp['JULD'][:]
         time = num2date(t, units = units, calendar='julian')
+        cycle_number = rootgrp['CYCLE_NUMBER'][:]
+        filepath = [str(p)] * lats.size
         first = False
     else:
         lats = ma.append(lats, rootgrp['LATITUDE'][:])
         lons = ma.append(lons, rootgrp['LONGITUDE'][:])
         t = rootgrp['JULD'][:]
         time = ma.append(time, num2date(t, units=units, calendar='julian'))
+        cycles = rootgrp['CYCLE_NUMBER'][:]
+        cycle_number = ma.append(cycle_number, cycles)
+        filepath = filepath + [str(p)] * len(cycles)
         
     rootgrp.close()
     
@@ -48,5 +53,7 @@ years = np.array([t.year for t in time.data])
 m = (months >= start_month) & (months <= end_month) & (years >= start_year) & (years <= end_year)
 
 # and write to a csv for loading into a GIS
-d = pd.DataFrame({'timestamp': time.data[m], 'month': months[m], 'year': years[m], 'lat': lats[m], 'lon': lons[m]})
+d = pd.DataFrame({'timestamp': time.data[m], 'month': months[m], 'year': years[m], 
+                  'lat': lats[m], 'lon': lons[m], 'cycle_number': cycle_number[m], 
+                  'filepath': np.array(filepath)[m]})
 d.to_csv(outputDir.joinpath('argo_positions.csv'), index=False)
